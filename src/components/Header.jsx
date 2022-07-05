3120f362f9d915de1c8b00f54ce1de2406f4160c
Julian Murphy <njmdistrisoft@gmail.com>
2022-07-05 20:05:37 -0300

    more styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,14 @@ const Header = () => {
   return (
     <div className="Header">
       <img src="https://picsum.photos/200/300"/>
-      <h1 className="Header-title">Julian Murphy</h1>
-      <h3 className="Header-job-title">Developer</h3>
-      <p className="Header-phone">+54 293129451</p>
-      <p className="Header-email">miemail@email.com</p>
-      <p className="Header-website">mi-site.com</p>
-      <p className="Header-address">Av. siempre viva 1234</p>
+      <div>
+        <h1 className="Header-title">Julian Murphy</h1>
+        <h3 className="Header-job-title">Developer</h3>
+        <p className="Header-phone">+54 293129451</p>
+        <p className="Header-email">miemail@email.com</p>
+        <p className="Header-website">mi-site.com</p>
+        <p className="Header-address">Av. siempre viva 1234</p>
+      </div>
     </div>
   );
 }
