d9f4dd82275e3bf277bd122409495a8d99da4ba4
Julian Murphy <njmdistrisoft@gmail.com>
2022-07-05 20:26:20 -0300

    add more styles

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Profile = () => {
-  return;
+  return (
+    <div className="Profile">
+      <h3 className="Profile-title">
+        Perfil
+      </h3>
+      <p className="Profile-desc">
+        Descripción
+      </p>
+    </div>
+  );
 }
 
 export default Profile;
