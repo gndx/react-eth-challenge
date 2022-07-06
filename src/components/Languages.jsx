b95fffce95b8ce4caf8b2b261e75ad95d612297c
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2022-07-05 23:11:35 -0500

    Languages tests checked

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Languages = () => {
-  return <div>Languages</div>;
+  return (
+    <content>
+      <h2 className="Languages-title">Languages</h2>
+      <article>
+        <h3 className="Languages-item">Spanish</h3>
+        <h3 className="Languages-item">English</h3>
+        <h3 className="Languages-item">Music</h3>
+      </article>
+    </content>
+  );
 };
 
 export default Languages;
