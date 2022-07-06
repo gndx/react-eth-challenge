1cdb915eb3b87afe10e99d40e5c3d2cb1c69b920
Armando Rivera <rivera.armando997@gmail.com>
2022-07-06 15:35:51 -0500

    Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Header.styl';
+
+export const Header = ({ name, position, phone, email, website, image }) => {
+  return (
+    <header className="Header-title">
+      <img src={image} alt={name} />
+      <h1>{name}</h1>
+
+      <h5>{position}</h5>
+
+      <div className="Header-details">
+        <a className="phone" href={`tel:${phone}`}>
+          {phone}
+        </a>
+        <a className="email" href={`mailto:${email}`}>
+          {email}
+        </a>
+        <a className="website" target="_blank" href={website}>
+          Website
+        </a>
+      </div>
+    </header>
+  );
+};
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,28 +1,36 @@
 import React from 'react';
 import '../styles/components/App.styl';
-import Header from '../components/Header';
-import About from '../components/About';
-import Profile from '../components/Profile';
-import Experience from '../components/Experience';
-import Academic from '../components/Academic';
-import Skills from '../components/Skills';
-import Interest from '../components/Interest';
-import Languages from '../components/Languages';
+import { Header } from '../components/Header';
+// import About from '../components/About';
+// import Profile from '../components/Profile';
+// import Experience from '../components/Experience';
+// import Academic from '../components/Academic';
+// import Skills from '../components/Skills';
+// import Interest from '../components/Interest';
+// import Languages from '../components/Languages';
+
+const user = {
+  name: 'Armando Rivera',
+  position: 'Frontend developer',
+  phone: '+52 5563411993',
+  email: 'rivera.armando0829@gmail.com',
+  website: 'https://www.armandorivera.tech',
+  image: 'https://www.armandorivera.tech/assets/images/armando-rivera.png',
+};
 
 const App = () => {
   return (
     <>
-      <Header>
-        <About />
-      </Header>
-      <Profile />
+      <Header {...user}></Header>
+      {/* <Header><About /></Header> */}
+      {/* <Profile />
       <Experience />
       <Academic />
       <Skills />
       <Interest />
-      <Languages />
+      <Languages /> */}
     </>
-  )
+  );
 };
 
 export default App;
