995379d1156b44fd6451645dd4d4e32c31895383
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2022-07-05 22:56:45 -0500

    Profile tests checked

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <article>
+      <h2 className="Profile-title">Profile</h2>
+      <p className="Profile-desc">
+        I'm a Front End web developer. I have experience on the implementation
+        of web applications using a wide range of technologies such as HTML,
+        CSS, JavaScript, TypeScript, React and React Native. I considere my self
+        with skills like tenacity, calm and persistence that have allowed me to
+        constantly become a better developer and expand my knowledge in this
+        area. I also enjoy and I am very open about teaching others.
+      </p>
+    </article>
+  );
 };
 
 export default Profile;
