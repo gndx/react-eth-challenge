2b91d7c1c28580273320b5258b196f79c6a0e6df
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2022-07-05 23:05:08 -0500

    Academic tests checked

--- src/components/Academic.jsx
+++ src/components/Academic.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Academic = () => {
-  return <div>Academic</div>;
+  return (
+    <content>
+      <h2 className="Academic-title">Academic</h2>
+      <article>
+        <h3 className="Academic-item">Software engineer</h3>
+        <h3 className="Academic-item">Ethereum Developer Program</h3>
+        <h3 className="Academic-item">Producer & Composer</h3>
+      </article>
+    </content>
+  );
 };
 
 export default Academic;
