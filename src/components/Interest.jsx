c45e27c8999c22a1db0d7977ded5d625184eb7e1
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2022-07-05 23:09:33 -0500

    Interest tests checked

--- src/components/Interest.jsx
+++ src/components/Interest.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Interest = () => {
-  return <div>Interest</div>;
+  return (
+    <content>
+      <h2 className="Interest-title">Interest</h2>
+      <article>
+        <h3 className="Interest-item">BlocChain</h3>
+        <h3 className="Interest-item">Invesments</h3>
+        <h3 className="Interest-item">CryptoCurrency</h3>
+      </article>
+    </content>
+  );
 };
 
 export default Interest;
