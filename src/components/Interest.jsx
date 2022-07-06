81c88ca7ffcf0ca7738bc41e0459642d66c95076
Julian Murphy <njmdistrisoft@gmail.com>
2022-07-06 00:06:24 -0300

    add more styles

--- src/components/Interest.jsx
+++ src/components/Interest.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import '../styles/components/Interest.styl';
 
 const Interest = () => {
-  return;
+  return (
+    <div className="Interest">
+      <h3 className="Interest-title">Interest</h3>
+      <div>
+        <p className="Interest-item">Item</p>
+        <p className="Interest-item">Item</p>
+        <p className="Interest-item">Item</p>
+        <p className="Interest-item">Item</p>
+        <p className="Interest-item">Item</p>
+        <p className="Interest-item">Item</p>
+      </div>
+
+    </div>
+  );
 }
 
 export default Interest;
--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import '../styles/components/Languages.styl';
 
 const Languages = () => {
-  return;
+  return (
+    <div className="Languages">
+      <h3 className="Languages-title">Languages</h3>
+      <div>
+        <p className="Languages-item">Item</p>
+        <p className="Languages-item">Item</p>
+        <p className="Languages-item">Item</p>
+        <p className="Languages-item">Item</p>
+        <p className="Languages-item">Item</p>
+        <p className="Languages-item">Item</p>
+      </div>
+
+    </div>
+  );
 }
 
 export default Languages;
