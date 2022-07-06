51b5232dc0268da9a56ba66d89aed7f04ad9122f
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2022-07-05 23:07:22 -0500

    Skills tests checked

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <content>
+      <h2 className="Skills-title">Skills</h2>
+      <article>
+        <h3 className="Skills-item">React</h3>
+        <h3 className="Skills-item">Next</h3>
+        <h3 className="Skills-item">Git</h3>
+      </article>
+    </content>
+  );
 };
 
 export default Skills;
