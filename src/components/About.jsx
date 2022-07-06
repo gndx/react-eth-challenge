991a56db11c4c0501228db4348576e9bc4a97b2e
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2022-07-05 23:02:10 -0500

    Experience tests checked

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
+    <content>
       <h2 className="About-title">FrontEnd Developer</h2>
       <article>
         <h3 className="About-item">+57 3004864294</h3>
         <h3 className="About-item">leotangram@gmail.com</h3>
         <h3 className="About-item">Bogotá</h3>
       </article>
-    </div>
+    </content>
   );
 };
 
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <content>
+      <h2 className="Experience-title">Experience</h2>
+      <article>
+        <h3 className="Experience-item">Frontend developer at Rappi</h3>
+        <h3 className="Experience-item">Frontend developer at Merqueo</h3>
+        <h3 className="Experience-item">Frontend developer at Grability</h3>
+      </article>
+    </content>
+  );
 };
 
 export default Experience;
