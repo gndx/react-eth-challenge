467b8a75903293b9450aeb5605aae7ee5244686e
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2022-07-06 18:12:54 -0300

    Error in progress bar

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import { MdFavoriteBorder } from 'react-icons/md';
+import { FiChevronsRight } from 'react-icons/fi';
+import { ProgressBar } from 'react-bootstrap';
+import db from '../../../data.json';
+import { Text, SkillsStyle, Item, SkillsItemTitle } from './styles';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function Skills() {
+function Skills() {
   return (
-    <button type='button'>
-      <MdFavoriteBorder size='32px' />
-      0
-      likes!
-    </button>
+
+    <SkillsStyle className='skills'>
+      <Text>
+        {' '}
+        <h2 className='Skills-title'>Skills</h2>
+        {db.data.skills.map((skills) => (
+          <>
+            <Item key={skills.id}>
+              <div className='Skills-item'>
+
+                <SkillsItemTitle>
+                  <FiChevronsRight size='26px' />
+                  {skills.name}
+                </SkillsItemTitle>
+
+                {' '}
+
+              </div>
+              {' '}
+            </Item>
+            <div className='progressBar'>
+              <ProgressBar now={skills.percentage} label={`${skills.percentage}% completed`} />
+            </div>
+          </>
+        ))}
+
+      </Text>
+
+    </SkillsStyle>
+
   );
 };
+
+export default Skills;
