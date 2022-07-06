693b7dea6e2115f35aca53133835d2c1850a4803
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2022-07-06 18:41:49 -0300

    Skills test passed

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { FiChevronsRight } from 'react-icons/fi';
-import { ProgressBar } from 'react-bootstrap';
 import db from '../../../data.json';
 import { Text, SkillsStyle, Item, SkillsItemTitle } from './styles';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Skills() {
   return (
@@ -13,24 +11,19 @@ function Skills() {
         {' '}
         <h2 className='Skills-title'>Skills</h2>
         {db.data.skills.map((skills) => (
-          <>
-            <Item key={skills.id}>
-              <div className='Skills-item'>
+          <Item key={skills.id}>
+            <div className='Skills-item'>
 
-                <SkillsItemTitle>
-                  <FiChevronsRight size='26px' />
-                  {skills.name}
-                </SkillsItemTitle>
-
-                {' '}
-
+              <SkillsItemTitle>
+                <FiChevronsRight size='26px' />
+                {skills.name}
+              </SkillsItemTitle>
+              <div style={{ border: '4px solid #2b3739', borderRadius: '15px', width: '300px' }}>
+                <div style={{ height: '24px', width: skills.percentage, backgroundColor: '#e3b3ab', borderRadius: '15px' }} />
               </div>
-              {' '}
-            </Item>
-            <div className='progressBar'>
-              <ProgressBar now={skills.percentage} label={`${skills.percentage}% completed`} />
             </div>
-          </>
+
+          </Item>
         ))}
 
       </Text>
