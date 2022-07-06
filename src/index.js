8ac1bd9be5dbbdee1ad61657bf2f35b7e13788a4
Rafael Livise <r.livise@hotmail.com>
2022-07-05 23:59:27 -0500

    :triangular_flag_on_post: [UPDATE] index to react v18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './containers/App';
-
-ReactDOM.render(<App />, document.getElementById('app'));
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
