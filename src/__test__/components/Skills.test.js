11ae10aadf535fb463ed0aa8836bb54a16bf09b0
Juamez <juanmendezs.a@gmail.com>
2022-07-07 01:54:06 -0500

    deleted unused component (mount)

--- src/__test__/components/Skills.test.js
+++ src/__test__/components/Skills.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import Skills from '../../components/Skills';
 
 describe('<Skills />', () => {
