80746d9b6635fc79216092a21c9047d9e0e3748b
Armando Rivera <rivera.armando997@gmail.com>
2022-07-07 17:22:37 -0500

    Fetch and render data

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,33 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/components/App.styl';
 import { Header } from '../components/Header';
 import { About } from '../components/About';
 import { Profile } from '../components/Profile';
 import { Experience } from '../components/Experience';
-import { user } from '../data/user-info';
 import { Academic } from '../components/Academic';
 import { Skills } from '../components/Skills';
 import { Interest } from '../components/Interest';
 import { Languages } from '../components/Languages';
-
+const URL = 'http://localhost:3000/data';
 const App = () => {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    const response = await fetch(URL);
+    const res = await response.json();
+    setUser(res);
+  }
+
   return (
     <>
-      <Header {...user}>
-        <About descriptionList={user.descriptionList} />
-      </Header>
-      <Profile description={user.description} />
-      <Experience experience={user.experience} />
-      <div className="mixSection">
-        <Academic academic={user.academic} />
-        <Skills skills={user.skills} />
-      </div>
-      <div className="mixSection">
-        <Interest interest={user.interest} />
-        <Languages languages={user.languages} />
-      </div>
-      {/*
-       */}
+      {user && (
+        <>
+          <Header {...user}>
+            <About descriptionList={user.descriptionList} />
+          </Header>
+          <Profile description={user.description} />
+          <Experience experience={user.experience} />
+          <div className="mixSection">
+            <Academic academic={user.academic} />
+            <Skills skills={user.skills} />
+          </div>
+          <div className="mixSection">
+            <Interest interest={user.interest} />
+            <Languages languages={user.languages} />
+          </div>
+        </>
+      )}
     </>
   );
 };
