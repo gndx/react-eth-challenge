40c4744a48ef461bb53a889fe0c041398fef6dfe
Rafael Livise <r.livise@hotmail.com>
2022-07-07 00:45:10 -0500

    :lipstick: [UPDATE] distibution Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,24 @@ function Header({ children }) {
   const [theme, changeTheme] = useTheme();
   return (
     <div className='Header'>
-      <input onClick={() => changeTheme(!theme)} type='checkbox' value={theme} />
-      <h1 className='Header-title'>Rafael Livise</h1>
-      <div>{children}</div>
+      <figure className='Header-image'>
+        <img src='' alt='' />
+      </figure>
+      <div className='Header-container'>
+        <div className='Header-title'>
+          <input onClick={() => changeTheme(!theme)} type='checkbox' value={theme} />
+          <h1>Rafael Livise Livise</h1>
+          <p className='Header-job-title'>Header-job-title</p>
+        </div>
+
+        <div className='Header-body'>
+          <p className='Header-phone'>+00-000000</p>
+          <p className='Header-email'>Header@email</p>
+          <p className='Header-website'>header.com</p>
+          <p className='Header-address'>Header-job-title</p>
+        </div>
+        <div>{children}</div>
+      </div>
     </div>
   );
 }
