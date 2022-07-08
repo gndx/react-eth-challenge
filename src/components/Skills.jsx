f045fbbd9eed65533da7a6a7df176837f1432367
Alejandro Medina <5almero5@gmail.com>
2022-07-08 17:27:28 -0500

    Update iterable skill

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import '../styles/App.css';
 import { Box } from './Box';
 
-export const Skills = ({ className }) => {
+export function Skills({ className, skillLilst }) {
+  const [skils, setSkils] = useState(skillLilst);
+
+  useEffect(() => {
+    setSkils(skillLilst);
+  }, [skillLilst]);
+
   return (
-    <Box className={className} classTitle="Skills-title" title={'Skills-title'}>
-      <div>
-        <p className={'Skills-item'}>Skills-item</p>
-        <p className={'Skills-item'}>Skills-item</p>
-        <p className={'Skills-item'}>Skills-item</p>
-      </div>
+    <Box className={className} classTitle="Skills-title" title="Skills-title">
+      <section>
+        {skils.map(({ name, percentage }) => (
+          <p key={name} className="Skills-item">
+            {`${name} - ${percentage}`}
+          </p>
+        ))}
+      </section>
     </Box>
   );
-};
+}
 
 Skills.propTypes = {
   className: PropTypes.string,
+  skillLilst: PropTypes.arrayOf(
+    PropTypes.exact({
+      name: PropTypes.string,
+      percentage: PropTypes.string,
+    })
+  ),
 };
 
 Skills.defaultProps = {
   className: '',
+  skillLilst: [
+    {
+      name: 'HTML5',
+      percentage: '75%',
+    },
+    {
+      name: 'CSS',
+      percentage: '25%',
+    },
+    {
+      name: 'JavaScript',
+      percentage: '85%',
+    },
+  ],
 };
 
 export default Skills;
