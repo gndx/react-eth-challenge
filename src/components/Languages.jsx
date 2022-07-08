e464e5417862acad54d437d027be48f48db173b7
Julian Murphy <njmdistrisoft@gmail.com>
2022-07-07 21:31:21 -0300

    delete div

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -5,23 +5,20 @@ const Languages = ({ data }) => {
   return (
     <div className="Languages">
       <h3 className="Languages-title">Languages</h3>
-      <div>
-        <ul>
-          {
-            data ?
-                data.map((language, index) => {
-                  return (
-                    <li key={index} className="Languages-item">
-                      {language.name} - {language.percentage}
-                    </li>
-                  )
-                })
-              :
-                <>Cargando data...</>
-          }
-        </ul>
-      </div>
-
+      <ul>
+        {
+          data ?
+              data.map((language, index) => {
+                return (
+                  <li key={index} className="Languages-item">
+                    {language.name} - {language.percentage}
+                  </li>
+                )
+              })
+            :
+              <>Cargando data...</>
+        }
+      </ul>
     </div>
   );
 }
