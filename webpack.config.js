548b438def55f46b73b0d04dbe2d283506f70ff4
Juansecode <juan.sebastianri28@gmail.com>
2022-07-08 01:02:56 -0500

    Add a envieroment variable for the link APIAdd an environment variable for the API hook

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -50,5 +51,6 @@ module.exports = {
       filename: 'assets/[name].css',
     }),
     new CleanWebpackPlugin(),
+    new Dotenv(['SERVER']),
   ],
 };
