44c248cd5ea65f54cddedb41cc397c48656a6841
jejimeneza <jimenez.ing.sis@gmail.com>
2022-07-08 21:21:05 -0500

    * Loading data from data.json

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,28 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/components/App.styl';
+import getData from '../utils/getData';
 import Header from '../components/Header';
-import About from '../components/About';
+//import jsonData from 'data.json';
+/*import About from '../components/About';
 import Profile from '../components/Profile';
 import Experience from '../components/Experience';
 import Academic from '../components/Academic';
 import Skills from '../components/Skills';
 import Interest from '../components/Interest';
-import Languages from '../components/Languages';
+import Languages from '../components/Languages';*/
+
+/*
+const { data } = await getData(
+  '../data.json'
+);*/
+
+
+
 
 const App = () => {
-  return (
-    <>
-      <Header>
-        <About />
-      </Header>
-      <Profile />
-      <Experience />
-      <Academic />
-      <Skills />
-      <Interest />
-      <Languages />
-    </>
-  )
+
+  const [data, setData] = useState(null);
+
+  const getCVData = async () => {
+    const { data }  = await getData('data.json');
+    setData(data);
+  };
+
+  useEffect(() => {
+    getCVData();
+  }, []);
+
+  if (!data) return null;
+  else {
+
+    return (
+      <>
+        <Header name={data.name}>
+          {/*<About />*/}
+        </Header>
+        {/*
+        <Profile />
+        <Experience />
+        <Academic />
+        <Skills />
+        <Interest />
+        <Languages />
+    */}
+      </>
+    )
+  }
 };
 
 export default App;
