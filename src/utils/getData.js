b174899e1a0a18ee4dce7e46c720df78549bed59
Carlos J. Ramirez <tomkat_cr@yahoo.com>
2022-07-09 00:16:12 -0500

    Create getData.js function to consume an API

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -2,15 +2,41 @@
 // with fectch(), it will say 'File not found: http://localhost:8080/data.json'
 // So we read it with import and if a parameter is passed to getData()
 // it's assumed to be an url, not a local file...
-import customData from '../../data.json';
-function getData(url=null) {
-    if(url) {
-        return fetch(`${url}`)
-            .then(response => response.json())
+
+export function getEnvironmentVar(envVarName) {
+    if (typeof process !== 'undefined' && typeof process.env[envVarName] !== 'undefined') {
+        return process.env[envVarName];
     }
-    var promise = new Promise( (resolve, reject) => {
+    if (typeof window[envVarName] !== 'undefined') {
+        return window[envVarName];
+    }
+    return null;
+}
+
+function getData(urlOriginal = null) {
+    // Promise to be returned in case of no URL al all
+    const promise = new Promise((resolve, reject) => {
+        const customData = {};
         resolve(customData);
     });
+    let url = urlOriginal;
+    if (!url) {
+        // If no URL were specified, try to get the API URL from an environment variable
+        url = getEnvironmentVar('REACT_APP_API_URL');
+    }
+    if (!url) {
+        // If no URL were specified, try with the default value... it should be the DEV environment
+        // so 'npm run server' must be running
+        url = 'http://localhost:3000/data';
+    }
+    if (url) {
+        // If the URL were specified, call the API with fetch to get the data
+        return fetch(`${url}`)
+            .then((response) => response.json())
+            .then((response) => ({ data: response }));
+    }
+    // If no URL al all, get a empty object
     return promise;
 }
+
 export default getData;
