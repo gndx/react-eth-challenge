bc5e1c9d760e310ead1b78cb797cb1660d658e15
Fabian Reyes <57076657+reyesmfabian@users.noreply.github.com>
2022-07-09 16:47:20 -0500

    finally

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import '../styles/components/Social.scss';
 
-const Social = (props) => {
+function Social(props) {
   const { socialNetworks } = props;
   return (
     <section>
-      <h2 className="social-title">Social Networks</h2>
-      <div className="social-box">
+      <h2 className='social-title'>Social Networks</h2>
+      <div className='social-box'>
         {socialNetworks.map((socialNetwork, index) => (
-          <div className="social-item" key={index}>
+          <div className='social-item' key={index}>
             <h3>{socialNetwork.name}</h3>
-            <img className='social-icon' src={socialNetwork.icon} alt="icon" />
+            <img className='social-icon' src={socialNetwork.icon} alt='icon' />
           </div>
         ))}
       </div>
       <hr />
     </section>
   );
-};
+}
 
 export default Social;
