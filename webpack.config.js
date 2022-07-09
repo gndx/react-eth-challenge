99385b7fa0e0f4da966bbf3741a7cc2862662569
nicolas-munoz01 <98959106+nicolas-munoz01@users.noreply.github.com>
2022-07-09 18:47:45 -0500

    Prod env

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
