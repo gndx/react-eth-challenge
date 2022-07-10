7febf14a73fe479bcd25d135c6fe54bb84b25b5d
Fernando Sproviero <fernandospr@gmail.com>
2022-07-10 20:49:46 -0300

    Fixing React version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './containers/App';
 
-const root = createRoot(document.getElementById('app'));
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById('app'));
