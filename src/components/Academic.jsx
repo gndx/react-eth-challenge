9ac4c86f1faa29e53a531a6d97c144c78e335ada
Gonzalo Daniel Aguilar <aguilar.daniel.gonzalo@gmail.com>
2022-07-11 08:10:40 -0300

    fix

--- src/components/Academic.jsx
+++ src/components/Academic.jsx
@@ -7,7 +7,7 @@ const Academic = () => {
       <h2 className="Academic-title">Academic</h2>
       <ul>
         <li className="Academic-item">Software engineer</li>
-        <li className="Academic-item">Ethereum Developer Program</li>
+        <li className="Academic-item">Ethereum Developer</li>
         <li className="Academic-item">Agile Coach</li>
       </ul>
     </Card>
