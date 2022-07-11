c156c40ac2252d08199092ab2707968c7ea03e14
Julian Murphy <njmdistrisoft@gmail.com>
2022-07-11 01:13:00 -0300

    profile

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -38,7 +38,7 @@ const App = () => {
           phone={list.phone}
         />
       </Header>
-      <Profile description={list.Profile} />
+      <Profile description={list.profile} />
       <Experience data={list.experience} />
       <Academic data={list.academic} />
       <Skills data={list.skills} />
