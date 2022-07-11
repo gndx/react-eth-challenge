7324752faed505cddcc60a0c5c8f5a3c232f0f80
Julian Murphy <njmdistrisoft@gmail.com>
2022-07-11 01:49:31 -0300

    test

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,4 +1,8 @@
 const getData = async (url) => {
+  if (process.env.LOCAL_DATA) {
+    console.log('date',Promise.resolve('../../data.json'))
+    return Promise.resolve('../../data.json');
+  }
   return fetch(url)
     .then((body) => body.json());
 };
