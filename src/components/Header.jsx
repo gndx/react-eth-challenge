c7fe200a6b83442a8bf7671aa4e8b61a390568ee
Alberto Beltran <albertobeltran@protonmail.com>
2022-07-11 06:14:50 -0500

    minor fixes - avatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = ({data}) => {
     return (
         <Heading className='container-columns header'>
             <div className='thumb'>
-                <img src='https://clipart.info/images/ccovers/1531010991winking-emoji-png.png'></img>
+                <img src={data.avatar}></img>
             </div>
             <div className='content'>
                 <h1 className='h2 title Header-title'>{data.name}</h1>
