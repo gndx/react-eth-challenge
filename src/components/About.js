ed7ff9cc441447d2bf9a288f39424fd3d778adaf
Ignacio Nicanor Sebastian Molina Portoles <imolina@intermediait.com>
2022-07-11 16:01:06 -0300

    v1.1

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
-const About = ({data}) => {
+const About = ({ data }) => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', marginBottom:20 }}>
+    <div style={{ display: 'flex', flexDirection: 'column', marginBottom: 20 }}>
       <span className="About-title">About</span>
 
       <span className="Header-job-title">{data.profession}</span>
-      <div>
+      <div className='about-info-wrapper'>
         <span className="Header-phone About-item">{data.phone}</span>
         <span className="Header-email About-item">{data.email}</span>
-        <span className="Header-website About-item">{data.website}</span>
+        <span className="Header-website About-item">
+          <a href={'https://' + data.website} target="_blank">{data.website}</a>
+        </span>
       </div>
       <div>
         <span className="Header-adress">{data.address}</span>
