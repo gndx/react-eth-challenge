7d2211a7257f40e3898064a8c4aba92c9d59168a
Alberto Beltran <albertobeltran@protonmail.com>
2022-07-11 04:57:16 -0500

    set up and rendered header props

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,15 @@ import styled from 'styled-components';
 import Heading from '../styles/Heading';
 import About from './About'
 
-const Header = () => {
+const Header = ({data}) => {
     return (
         <Heading className='container-columns header'>
             <div className='thumb'>
                 <img src='https://clipart.info/images/ccovers/1531010991winking-emoji-png.png'></img>
             </div>
             <div className='content'>
-                <h1 className='h2 title Header-title'>.Header-title</h1>
-                <About />
+                <h1 className='h2 title Header-title'>{data.name}</h1>
+                <About data={data}/>
             </div>
         </Heading>
     )
