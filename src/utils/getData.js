dce4f27de335a9e154e7ba383075aea1361ab4b0
Julian Murphy <njmdistrisoft@gmail.com>
2022-07-11 01:50:08 -0300

    roll data

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,8 +1,4 @@
 const getData = async (url) => {
-  if (process.env.LOCAL_DATA) {
-    console.log('date',Promise.resolve('../../data.json'))
-    return Promise.resolve('../../data.json');
-  }
   return fetch(url)
     .then((body) => body.json());
 };
