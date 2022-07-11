3be1cdfdf682ab35f6e7fad25338d367e7185a50
Julian Murphy <njmdistrisoft@gmail.com>
2022-07-10 22:29:46 -0300

    lint header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import '../styles/components/Header.styl';
 
-const Header = ({children: about, fullName, profileImage}) => {
+const Header = ({ children: about, fullName, profileImage }) => {
   return (
-    <header className="Header">
-      <img src={profileImage} />
+    <header className='Header'>
+      <img src={profileImage} alt={`${fullName} profile avatar`} />
       <div>
-        <h1 className="Header-title">{fullName}</h1>
+        <h1 className='Header-title'>{fullName}</h1>
         {about}
       </div>
     </header>
   );
-}
+};
 
 export default Header;
