807ede5433554ce41e10dd04eb45d4efaed9b037
Alberto Beltran <albertobeltran@protonmail.com>
2022-07-11 03:38:00 -0500

    [gndx#4] integrated API with App.jsx using useState and useEffect

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/components/App.css';
 import Header from '../components/Header';
 import About from '../components/About';
@@ -8,8 +8,19 @@ import Academic from '../components/Academic';
 import Skills from '../components/Skills';
 import Interest from '../components/Interest';
 import Languages from '../components/Languages';
+import getData from '../utils/getData';
 
 const App = () => {
+  const [cvData, setCvData] = useState({});
+
+  useEffect(() => {
+    getData('http://127.0.0.1:3000/data')
+    .then(data => setCvData(data))
+    .catch(error => console.log(error));
+  }, [])
+
+  console.log(cvData)
+
   return (
     <div className='container my-4'>
       <div className='row mb-3'>
