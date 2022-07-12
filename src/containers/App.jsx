af103e7f30ab69e08cbf321e1e8989034503d68f
Ruben Dario Troche Piñanez <107197505+tatita2021@users.noreply.github.com>
2022-07-12 12:08:49 -0400

    Update App.jsx
    Modificaciones realizadas al objeto App.jsx, cómo parte del reto de react-eth-challenge

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,28 +1,69 @@
-import React from 'react';
+import React, {useEffect, useState} from "react";
 import '../styles/components/App.styl';
-import Header from '../components/Header';
-import About from '../components/About';
-import Profile from '../components/Profile';
-import Experience from '../components/Experience';
-import Academic from '../components/Academic';
-import Skills from '../components/Skills';
-import Interest from '../components/Interest';
-import Languages from '../components/Languages';
+import Header from "../components/Header";
+import About from "../components/About";
+import Skills from "../components/Skills";
+import Interest from "../components/Interest";
+import Languages from "../components/Languages";
+import getdata from '../utils/getData'
+
 
 const App = () => {
+  const [user, setUser] = useState(null);
+    useEffect(() => {
+      getuseData ();
+    }, []);
+
+  const getUserData = async () => {
+    const data = await getData(
+      process.env.USER_DATA || 'http://localhost:3000/data');
+    setUser(data);
+  };
+  if (!user) return null;
+
+  const {
+    Academic: academic,
+    address,
+    avatar,
+    email,
+    experience,
+    interest,
+    languages,
+    name,
+    phone,
+    profession,
+    Profile: profile,
+    skills,
+    website,
+  } = user;
+
   return (
-    <>
-      <Header>
-        <About />
-      </Header>
-      <Profile />
-      <Experience />
-      <Academic />
-      <Skills />
-      <Interest />
-      <Languages />
-    </>
-  )
+    <div ClassName='App'>
+      <div ClassName='App-container'>
+        <Header avatar={avatar} name={name}>
+          <About
+            address={address}
+            email={email}
+            phone={phone}
+            profession = {profession}
+            website = {website}
+           />
+        </Header>
+        <Profile profile={profile}/>
+        <Experience experience={experience}/>
+        <div ClassName='App-grid'>
+          <Academic academic={academic}/>
+          <Skills skills={skills}/>
+        </div>
+        <div ClassName='App-grid'>
+          <Interest interest={interest}/>
+          <Languages languages={languages}/>
+        </div>
+      </div>
+      <div ClassName='App-round App-top-left'/>
+      <div ClassName='App-round App-bottom-right'/>
+    </div>
+  );
 };
 
 export default App;
