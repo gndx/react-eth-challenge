e956adb03faab5f9c13a4d177e839fe5db5d7382
Juamez <juanmendezs.a@gmail.com>
2022-07-12 18:53:44 -0500

    update getData function

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -19,7 +19,6 @@ function App() {
     }
     getDataApi();
   }, []);
-
   return (
     <>
       <Header
--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,8 +1,8 @@
-const getData = async (url = 'http://localhost:3000/data/') => {
+const getData = async (url = 'https://raw.githubusercontent.com/Juamez/react-eth-challenge/main/data.json') => {
   try {
     const res = await fetch(url);
     const data = await res.json();
-    return data;
+    return data.data;
   } catch (e) {
     throw new Error('Failed Call');
   }
