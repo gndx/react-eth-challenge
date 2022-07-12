1c0123fc60e9e098b54b85d2bb13e714c6a39605
JonathanDVZ <jodaviza@gmail.com>
2022-07-11 23:19:43 -0300

    i18n changes

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { FirebaseAppProvider } from 'reactfire';
 import { I18nextProvider } from 'react-i18next';
 import firebaseConfig from '@/firebaseConfig';
 import i18n from '@/i18n';
-import getData from '@/utils/getData';
 import '@/assets/styles/components/App.scss';
 import Header from '@/components/Header';
 import About from '@/components/About';
@@ -14,24 +13,9 @@ import Skills from '@/components/Skills';
 import Interest from '@/components/Interest';
 import Languages from '@/components/Languages';
 
-const App = () => {
-  const [locales, setLocales] = useState({});
-
-  useEffect(() => {
-    (async () => {
-      const locales = await getData(
-        'https://storage.googleapis.com/briefcase-6e790.appspot.com/locales/data.json'
-      );
-
-      setLocales(locales);
-    })();
-  }, []);
-
-  if (Object.keys(locales).length === 0) return <div className='w-100 h-100 d-flex justify-content-center align-items-center position-absolute'><div className="lds-ripple"><div></div><div></div></div></div> ;
-
-  return (
+const App = () => (
     <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-    <I18nextProvider i18n={i18n(locales)}>
+    <I18nextProvider i18n={i18n}>
       <div className="app-container d-flex flex-column position-relative">
         <Header>
           <Profile />
@@ -48,6 +32,5 @@ const App = () => {
     </I18nextProvider>
   </FirebaseAppProvider>
   );
-};
 
 export default App;
--- src/i18n.js
+++ src/i18n.js
@@ -1,22 +1,20 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import resources from '../data.json';
 
-const i18nInit = (resources) => {
-  i18n
-    .use(initReactI18next) // passes i18n down to react-i18next
-    .init({
-      // the translations
-      // (tip move them in a JSON file and import them,
-      // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
-      resources,
-      lng: 'en', // if you're using a language detector, do not define the lng option
-      fallbackLng: 'en',
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+    resources,
+    lng: 'en', // if you're using a language detector, do not define the lng option
+    fallbackLng: 'en',
 
-      interpolation: {
-        escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
-      }
-    });
-  return i18n;
-};
+    interpolation: {
+      escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    }
+  });
 
-export default i18nInit;
+export default i18n;
