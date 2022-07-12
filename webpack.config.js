1764e772d466e8dba7f4a14d311863d743184937
julian camilo garcia <camilo.garci@rappi.com>
2022-07-12 01:19:37 -0500

    final commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
 module.exports = {
   entry: './src/index.js',
@@ -39,6 +41,14 @@ module.exports = {
         ],
       },
     ],
+  },
+  devServer: {
+    disableHostCheck: true,
+    contentBase: './ dist',
+    compress: true,
+    inline: true,
+    port: server_port,
+    host: server_host,
   },
   plugins: [
     new HtmlWebPackPlugin({
