241835b83fbff851a695be13b3237e56d19dcd93
gonza.otc <gonza.otc@gmail.com>
2022-07-12 22:53:58 -0300

    fixing interest title bug

--- PULL_REQUEST_TEMPLATE.md
+++ PULL_REQUEST_TEMPLATE.md
@@ -18,7 +18,7 @@ Deploy: https://react-eth-challenge-alpha.vercel.app/
 - [ ] Septimo Problema
 
 #### Documentation:
-- I made a high order component (<Section/>) to renderice the different components and reuse structure. 
+- I made a high order component (Section) to renderice the different components and reuse structure. 
 - I built the json data structure to be easy adaptable to multi language, I will add a languague switcher later in the CV.
 - I integrated a welook link to show my POAP's as part of my resume.
 - I made a small project-viewer component, which later will be translated into a carousel to slide right and left. 
--- src/components/Interest.jsx
+++ src/components/Interest.jsx
@@ -3,16 +3,16 @@ import Icon from '../containers/Icon';
 import Section from '../containers/Section';
 
 const Interest = ({
-  data = { interest: { title: 'interest', interests: [{}, {}, {}] } },
+  data = { interest: { title: '', interests: [{}, {}, {}] } },
 }) => {
   return (
     <Section id="Interest" className="flex flex-col">
       <span className="flex items-center mb-3">
         <Icon name="Fire" className="mr-0.5 text-red-500" />
-        <h3 className="Interest-title">{data.interest.title}</h3>
+        <h3 className="Interest-title">{data.interest?.title}</h3>
       </span>
       <div className="flex flex-col">
-        {data.interest.interests.map((interest, index) => (
+        {data.interest?.interests.map((interest, index) => (
           <p className="Interest-item mb-2" key={index}>
             {interest}
           </p>
