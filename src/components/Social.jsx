636b3a8913d3694b2c0e892a60d9ec177d3976bb
Tini Cassi <89404026+agustinacassi@users.noreply.github.com>
2022-07-13 16:13:42 -0300

    Update Social.jsx

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -11,8 +11,8 @@ const Social = () => {
       {data.social? (
         data.social.map((info, i) => {
           return (
-            <ul>
-            <li key={i}>
+            <ul key={i}>
+            <li>
              <h3>{info.name}</h3><br></br>
              <small>{info.url}</small>
             </li>
