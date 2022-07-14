b621f175b0060da977bcf3c5f03c5d67668dc961
Candela Bramucci <candelabramucci05@gmail.com>
2022-07-13 23:15:10 -0300

    adding effects to navbar on desktop

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Nav({navVisible}){
+    const [navColor, setNavColor] = useState(false)
+
+    window.addEventListener('scroll', ()=>{
+      if(window.scrollY >= 150) setNavColor(true)
+      else setNavColor(false)
+    })
+
     return(
-        <nav className='main-nav' id="Main-nav" data-visible={navVisible}>
+        <nav className={navColor? 'main-nav main-nav__background' : 'main-nav'} id="Main-nav" data-visible={navVisible}>
         <a href='#Home'>Home</a>
         <a href='#About'>About Me</a>
         <a href='#Skills'>Skills</a>
