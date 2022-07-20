9f123c76e3c7951d2cbbf834d19e872c7dba49b2
cmarchese <cmarchesetdiii@gmail.com>
2022-07-19 21:05:51 -0300

    trying to work out the deployment problems

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,7 +1,7 @@
 const getData = (url) => {
   return fetch(url)
-    .then((response) => response.json());
-    .then((data) => data.data);
+    .then((response) => response.json())
+    .then((data) => data.data)
 };
 
 export default getData;
