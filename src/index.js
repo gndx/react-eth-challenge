9e1e4d262ae139c3bebd96b65a7cdbc35e47340b
salviega <salviega6@gmail.com>
2022-07-26 16:23:08 -0500

    [FEAT] React 18 a React 16

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById('app'));
