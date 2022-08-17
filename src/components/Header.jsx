2b4a9d1c890dd52c60ef149667ab37b8902fadf4
gonza.otc <gonza.otc@gmail.com>
2022-08-16 23:09:04 -0300

    Changes on buttons and descriptions in json

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Section from '../containers/Section.jsx';
 import Icon from '../containers/Icon.jsx';
+import Welook from './Welook.jsx';
+import Curriculum from './Curriculum.jsx';
 
 const Header = ({
   data = {
@@ -71,28 +73,9 @@ const Header = ({
         </div>
       </div>
 
-      <div className="ml-auto max-w-[232px] self-start bg-white/10 border-white/40 border-2 py-3 px-3 rounded-lg shadow-lg hidden lg:inline">
-        <span>
-          You can see my POAP's in my social wallet on welook{' '}
-          {/* <img
-            className="w-16 inline relative bottom-[4px]"
-            src="https://www.welook.io/static/media/welook-logo-main.356ecc8f4e93d5e7435ef73cca5d7e33.svg"
-            alt="welook logo"
-          /> */}
-        </span>
-        <a
-          href="https://www.welook.io/0x7ED27AB6cE44B19e2c1eE1317B836D4dEC1fD7ae"
-          target="_blank"
-          className="btn--pink mt-6"
-          draggable="false"
-        >
-          See poaps{' '}
-          <img
-            className="w-5 inline relative top-[1px] ml-1"
-            src="https://poap.xyz/POAP.f74a7300.svg"
-            alt="poap logo"
-          />
-        </a>
+      <div className="flex flex-col ml-auto self-start w-full sm:max-w-[232px] mt-8 sm:mt-0">
+        <Welook />
+        {/* <Curriculum /> */}
       </div>
     </Section>
   );
