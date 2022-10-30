4596dd5c2ec075ece02086e7769743e71f376d43
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2022-10-30 15:39:25 +0100

    change render to ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './containers/App';
 
+ReactDOM.render(<App />, document.getElementById('app'));
 
-const container = document.getElementById('app');
-const root = createRoot(container);
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+// import App from './containers/App';
 
-root.render(<App />)
+
+// const container = document.getElementById('app');
+// const root = createRoot(container);
+
+// root.render(<App />)
